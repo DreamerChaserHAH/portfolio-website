44b7f99c86262a8ed66ca2c2649463a1794c3125
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2023-02-17 21:38:19 +0630

    deleting contact

--- portfolio_backend/schemas/index.js
+++ portfolio_backend/schemas/index.js
@@ -3,6 +3,5 @@ import testimonials from './testimonials'
 import experiences from './experiences'
 import skills from './skills'
 import workExperience from './workExperience'
-import contact from './contact'
 
-export const schemaTypes = [works, testimonials, skills, workExperience, experiences, contact]
+export const schemaTypes = [works, testimonials, skills, workExperience, experiences]
